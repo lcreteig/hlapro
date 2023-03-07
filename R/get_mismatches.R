b3080702e7b6461e22b93d9013124921c8e02a41
Leon Reteig <leonreteig@gmail.com>
2023-03-07 10:52:30 -0500

    Add docs and spellcheck

--- R/get_mismatches.R
+++ R/get_mismatches.R
@@ -1,3 +1,24 @@
+#' Determine mismatched antigens from typings
+#'
+#' `get_mismatches()` returns mismatched HLAs given a donor and recipient HLA
+#' typing. That is, those antigens that occur in the *donor* but *not* the
+#' recipient typing.
+#'
+#' @param donor,recipient A pair of strings containing the donor and recipient
+#'   HLA typing, respectively. Each antigen must be separated by a space.
+#'
+#' @return A string with the mismatched antigens (separated by spaces).
+#' @export
+#'
+#' @examples
+#' donor <- "A1 A2"
+#' recipient <- "A1 A2"
+#' get_mismatches(donor, recipient)
+#' #> ""
+#' get_mismatches("A1 A2", "A3 A4")
+#' #> "A1 A2"
+#' get_mismatches("A1 A2 B5", "A3 A4 B5 B12")
+#' #> "A1 A2"
 get_mismatches <- function(donor, recipient) {
   setdiff(
     # separate alleles by spaces
