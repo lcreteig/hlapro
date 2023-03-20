eb21278dff1e2f3830aab57810c4ad290d956e1d
Leon Reteig <leonreteig@gmail.com>
2023-03-20 15:17:38 -0400

    Make locus an argument

--- R/extract_alleles.R
+++ R/extract_alleles.R
@@ -1,18 +1,27 @@
-extract_alleles <- function(df, col_typing) {
-  # TODO: vectorize with locus argument, and named list that can be indexed with it
-  anything <- ".*" # at the start
-  neg_nmdp <- "(?<![:A-Z])" # don't match NMDP Multiple Allele codes
-  a <- r"(A\*?)" # locus
-  allele <- r"(\S+)" # allele
-  inbetween <- r"((?:\s(?!A)\S+)*\s?)" # other HLAs inbetween
+extract_alleles <- function(df, col_typing, locus = c("A")) {
+  locus <- rlang::arg_match(locus)
 
-  pattern <- c(anything,
-    neg_nmdp, a,
-    A_1 = allele,
-    inbetween,
-    neg_nmdp, a,
-    A_2 = allele
+  regexps <- list(
+    anything = ".*", # at the start
+    neg_nmdp = "(?<![:A-Z])", # don't match NMDP Multiple Allele codes
+    allele = r"(\S+)", # allele
+    loci = list(
+      A = r"(A\*?)"
+                 ),
+    inbetween = list( # other HLAs inbetween
+      A = r"((?:\s(?!A)\S+)*\s?)"
+                     ))
+
+  pattern <- c(regexps$anything,
+    regexps$neg_nmdp, regexps$loci[[locus]],
+    regexps$allele,
+    regexps$inbetween[[locus]],
+    regexps$neg_nmdp, regexps$loci[[locus]],
+    regexps$allele
   )
+
+  names(pattern)[4] <- paste0(locus,"_1")
+  names(pattern)[8] <- paste0(locus,"_2")
 
   tidyr::separate_wider_regex(df, {{ col_typing }},
     patterns = pattern,
--- tests/testthat/test-extract_alleles.R
+++ tests/testthat/test-extract_alleles.R
@@ -1,72 +1,72 @@
 test_that("both HLA-A alleles are extracted from beginning", {
   df_in <- tidyr::tibble(typing = "A1 A2 B7 B8 DR1 DQ5")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = "2", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("both HLA-A alleles are extracted from end", {
   df_in <- tidyr::tibble(typing = "B7 B8 DR1 DQ5 A1 A2")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = "2", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("both HLA-A alleles are extracted from middle", {
   df_in <- tidyr::tibble(typing = "B7 B8 A1 A2 DR1 DQ5")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = "2", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("both HLA-A alleles are extracted when separated by others", {
   df_in <- tidyr::tibble(typing = "A1 B7 B8 A2 DR1 DQ5")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = "2", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("second allele is NA when only 1 HLA-A allele is present", {
   df_in <- tidyr::tibble(typing = "B7 A1 B8")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
   df_in <- tidyr::tibble(typing = "A1 B7 B8")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
   df_in <- tidyr::tibble(typing = "B7 B8 A1")
   df_out <- tidyr::tibble(A_1 = "1", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("both alleles are NA when no HLA-A allele is present", {
   df_in <- tidyr::tibble(typing = "B7 B8 DR1 DQ5")
   # TODO: consider turning this into a true NA in the function itself
   df_out <- tidyr::tibble(A_1 = "", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("double-digit HLA-A alleles are extracted", {
   df_in <- tidyr::tibble(typing = "A10 A19 B7")
   df_out <- tidyr::tibble(A_1 = "10", A_2 = "19", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("high resolution HLA-A alleles are extracted", {
   df_in <- tidyr::tibble(typing = "A*01:01 A*01:02 B07*01:02 B07*01:02")
   df_out <- tidyr::tibble(A_1 = "01:01", A_2 = "01:02", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("intermediate resolution HLA-A alleles are extracted", {
   df_in <- tidyr::tibble(typing = "A*02:HBMC B*08:NMTJ")
   df_out <- tidyr::tibble(A_1 = "02:HBMC", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
 
 test_that("'A's in MACs are not extracted", {
   df_in <- tidyr::tibble(typing = "A*02:HBMC DRB1*04:AMR B*08:NMTJ")
   df_out <- tidyr::tibble(A_1 = "02:HBMC", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
   df_in <- tidyr::tibble(typing = "A*02:HBMC B*08:BAD")
   df_out <- tidyr::tibble(A_1 = "02:HBMC", A_2 = NA_character_, df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
   df_in <- tidyr::tibble(typing = "A*01:AABJE A*02:HBMC")
   df_out <- tidyr::tibble(A_1 = "01:AABJE", A_2 = "02:HBMC", df_in)
-  expect_equal(extract_alleles(df_in, typing), df_out)
+  expect_equal(extract_alleles(df_in, typing, locus = "A"), df_out)
 })
