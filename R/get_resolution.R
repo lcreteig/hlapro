5b61941cf6bdd636a6d6fd67177346c4b4ac73ef
Leon Reteig <leonreteig@gmail.com>
2023-03-27 16:47:42 -0400

    Handle XX codes; add test for g/p groups

--- R/get_resolution.R
+++ R/get_resolution.R
@@ -1,9 +1,17 @@
 get_resolution <- function(allele) {
+  # N.B. assumes all ambiguities are intermediate, even when the ambiguity is >2 field codes
   dplyr::case_when(
-    # "*" followed by capital letter or digits and then a slash
-    stringr::str_detect(allele, r"(\*\d+:?([A-Z]|\d+\/))") ~ "intermediate",
+    # "*" followed by capital letter (but not XX), or digits and then a slash
+    stringr::str_detect(
+      allele,
+      r"(\*\d+:?(?!XX)([A-Z]|\d+\/))"
+    ) ~ "intermediate",
     # "*" followed by 4 digits with optional semicolon in between
-    stringr::str_detect(allele, r"(\*\d{2}:?\d{2})") ~ "high",
+    # TODO: return field code with high? (but some already at allelic resolution with 4 fields)
+    stringr::str_detect(
+      allele,
+      r"(\*\d{2}:?\d{2})"
+    ) ~ "high",
     is.na(allele) ~ NA_character_,
     .default = "low"
   )
