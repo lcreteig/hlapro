0dbe3b45002ced6d7ac9dfae64e35dc7919e5d20
Leon Reteig <leonreteig@gmail.com>
2023-03-28 17:00:47 -0400

    Test prefixing and whether it's vectorized

--- tests/testthat/test-validate_allele.R
+++ tests/testthat/test-validate_allele.R
@@ -1,3 +1,5 @@
+# TODO: add false expectations (invalid HLAs)
+
 test_that("serological/antigen notation is valid", {
   expect_true(validate_allele("A2"))
   expect_true(validate_allele("A80"))
@@ -33,6 +35,11 @@ test_that("MACs are valid", {
   expect_true(validate_allele("DPB1*04BDVU"))
 })
 
+test_that("prefixing is allowed", {
+  expect_true(validate_allele("HLA-A*01:01"))
+  expect_true(validate_allele("HLA-DRB1*10:03"))
+})
+
 test_that("ambiguities are allowed", {
   expect_true(validate_allele("A*74:06/A*22:03N"))
   expect_true(validate_allele("A*74:06/22:03"))
@@ -66,4 +73,17 @@ test_that("P groups are valid", {
 test_that("G groups are valid", {
   expect_true(validate_allele("A*01:01:01G"))
   expect_true(validate_allele("DPB1*879:01:01G"))
+})
+
+test_that("vectors work", {
+  expect_equal(
+    validate_allele(c("A2", "A*01:AABJE", "A*24:02:01:02L", "not")),
+    c(TRUE, TRUE, TRUE, FALSE)
+  )
+})
+
+test_that("mutate in dataframe works", {
+  df <- tidyr::tibble(alleles = c("A2", "A*01:AABJE", "A*24:02:01:02L", "not"))
+  df <- dplyr::mutate(df, alleles_check = validate_allele(alleles))
+  expect_equal(df$alleles_check, c(TRUE, TRUE, TRUE, FALSE))
 })
