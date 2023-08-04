810a995a2081799f033ffc7a1e5920a16e13ce8f
Leon Reteig <leonreteig@gmail.com>
2023-08-04 12:22:08 -0400

    Deal with split/broad, remove "g"

--- R/_upscaling.R
+++ R/_upscaling.R
@@ -2,42 +2,40 @@ library(tidyverse)
 library(readxl)
 library(hlapro)
 
-typing <- "A1 A2 B7 B8 DR1 DR7"
-
-typing_alleles <- extract_alleles_str(typing, strip_locus = FALSE, loci = c("A", "B", "DRB1"))
-
 haplo_df <- read_xlsx("~/Downloads/A~C~B~DRB3-4-5~DRB1~DQB1.xlsx")
 
-# compatible_haplos <- haplo_df |>
-#   select(A, C, B, `DRB3-4-5`, DRB1, DQB1, EURCAU_freq, EURCAU_rank) |>
-#   filter(get_serology(A) %in% typing_alleles[c("A_1", "A_2")],
-#          get_serology(B) %in% typing_alleles[c("B_1", "B_2")],
-#          get_serology(DRB1) %in% typing_alleles[c("DRB1_1", "DRB1_2")]) |>
-#   mutate(haplo = str_c(A, B, C, `DRB3-4-5`, DRB1, DQB1, sep = "~"), .before = EURCAU_freq)
-#
-# expand(compatible_haplos, haplo, haplo)
-
 top_haplos_ser <- haplo_df |>
-  slice_max(EURCAU_freq, n = 5000) |>
-  mutate(haplo = str_c(A, B, C, `DRB3-4-5`, DRB1, DQB1, sep = "~"), .before = EURCAU_freq) |>
-  select(A, B, DRB1, haplo, EURCAU_freq, EURCAU_rank) |>
-  mutate(across(c(A, B, DRB1), ~ get_serology(.x)))
+  mutate(across(c(A, B, C, `DRB3-4-5`, DRB1, DQB1), ~ str_remove(.x, "g"))) |> # remove "g" suffix
+  slice_max(EURCAU_freq, n = 5000) |> # cf. BW, pick top 5000 haplos
+  select(A, B, C, `DRB3-4-5`, DRB1, DQB1, EURCAU_freq, EURCAU_rank) |>
+  # translate alleles to serological equivalents
+  mutate(across(c(A, B, DRB1), ~ get_broad(.x), .names = "{.col}_broad"),
+         across(c(A, B, DRB1), ~ get_split(.x), .names = "{.col}_split"))
 
-comp_haplos <- top_haplos_ser |>
-  filter(
-    A %in% typing_alleles[c("A_1", "A_2")],
-    B %in% typing_alleles[c("B_1", "B_2")],
-    DRB1 %in% typing_alleles[c("DRB1_1", "DRB1_2")]
-  )
+typing <- "A24 A28 B35 B61 DR4 DR11"
 
+typing_alleles <- extract_alleles_str(typing,
+                                      strip_locus = FALSE,
+                                      loci = c("A", "B", "DRB1"))
+
+comp_haplos <- top_haplos_ser |> # keep only haplos with alleles compatible with typing
+  filter(A_broad %in% typing_alleles | A_split %in% typing_alleles,
+         B_broad %in% typing_alleles | B_split %in% typing_alleles,
+         DRB1_broad %in% typing_alleles | DRB1_split %in% typing_alleles)
+
+# combine all permutations of haplos (i.e., make phased genotypes)
 cross_join(comp_haplos, comp_haplos, suffix = c("_1", "_2")) |>
+  # make genotype out of serological equivalents of alleles in phased genotype
   mutate(genotype_ser = pmap(
-    .l = list(A_1, A_2, B_1, B_2, DRB1_1, DRB1_2),
+    .l = list(A_broad_1, A_broad_2, A_split_1, A_split_2,
+              B_broad_1, B_broad_2, B_split_1, B_split_2,
+              DRB1_broad_1, DRB1_broad_2, DRB1_split_1, DRB1_split_2),
     .f = c
-  )) |>
+  )) |> # keep only phased genotypes that are same as input typing
   filter(map_lgl(genotype_ser, \(x) all(typing_alleles %in% x))) |>
   mutate(
     phased_freq = 2 * EURCAU_freq_1 * EURCAU_freq_2,
     likelihood = phased_freq / sum(phased_freq)
   ) |>
   slice_max(likelihood)
+
