8e9565464fa5332de21b252718b1c505f4100361
Leon Reteig <leonreteig@gmail.com>
2023-08-24 15:38:48 -0400

    Add utils to manage download(ed) files

--- R/eplet_registry.R
+++ R/eplet_registry.R
@@ -1,3 +1,53 @@
+load_eplet_registry <- function(folder_path = NULL,
+                                filename = NULL,
+                                print_version = TRUE,
+                                return_path = FALSE,
+                                delete = FALSE) {
+  if (is.null(folder_path)) {
+    folder_path <- tools::R_user_dir("hlapro", "cache")
+  }
+
+  if (return_path) {
+    return(folder_path)
+  }
+
+  if (is.null(filename)) {
+    filename <- "eplets.rds"
+  }
+
+  file_path <- file.path(folder_path, filename)
+
+  if (delete) {
+    unlink(file_path)
+    return(invisible())
+  }
+
+  if (!dir.exists(folder_path)) {
+    dir.create(folder_path)
+  }
+
+  if (file.exists(file_path)) {
+    df_eplet <- readRDS(file_path)
+    if (print_version) {
+      message(
+        stringr::str_glue(
+          "Loaded Eplet Registry table ({attr(df_eplet, 'notes')}),\n",
+          "released {attr(df_eplet, 'date')}, ",
+          "downloaded from {attr(df_eplet, 'url')}"
+        )
+      )
+    }
+
+    return(invisible(df_eplet))
+  }
+
+  if (rlang::is_interactive() && scrape_permission() == 2) {
+    return(invisible())
+  }
+
+  invisible(scrape_eplet_registry(file_path))
+}
+
 fetch_registry_version <- function() {
   registry_url <- "https://www.epregistry.com.br"
   version_text <- rvest::read_html(registry_url) |>
@@ -17,7 +67,18 @@ fetch_registry_version <- function() {
   ))
 }
 
-scrape_eplet_registry <- function(base_url, databases) {
+scrape_permission <- function() {
+  q_title <- paste(
+    "Do you want to download the HLA Eplet Registry tables",
+    "(to lookup which eplets occur on which alleles, and vice versa?)"
+  )
+
+  utils::menu(choices = c("Yes", "No"), title = q_title)
+}
+
+scrape_eplet_registry <- function(file_path) {
+  base_url <- "https://www.epregistry.com.br/index/databases/database/"
+  databases <- c("ABC", "DRB", "DQ", "DP", "DRDQDP")
   # CSS selector paths to the individual columns
   # (scraping entire table with rvest::html_table resulted in misaligned rows/
   # columns)
@@ -40,13 +101,17 @@ scrape_eplet_registry <- function(base_url, databases) {
 
   # scrape all columns, add each to a list, and store the database used
   scrape_table <- function(base_url, database, col_paths) {
+    Sys.sleep(0.5) # wait a little between scrapes
     page_html <- rvest::read_html(paste0(base_url, database))
     purrr::map(col_paths, \(x) scrape_column(page_html, x)) |>
       purrr::list_assign(database = database)
   }
 
   # for each database (i.e. page), scrape all columns, store in another list
-  df <- purrr::map(databases, \(x) scrape_table(base_url, x, col_paths)) |>
+  df <- purrr::map(databases,
+    \(x) scrape_table(base_url, x, col_paths),
+    .progress = "Collecting tables from HLA Eplet Registry website"
+  ) |>
     purrr::map(tidyr::as_tibble) |> # make a dataframe out of each scraped db
     purrr::list_rbind() |> # combine into one dataframe
     # clean up the column: text always starts with "Yes" if eplet confirmed
@@ -67,5 +132,5 @@ scrape_eplet_registry <- function(base_url, databases) {
   attr(df, "notes") <- registry_info[["notes"]]
   attr(df, "url") <- registry_info[["url"]]
 
-  df
+  saveRDS(df, file_path)
 }
