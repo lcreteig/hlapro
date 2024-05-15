987ebf850d31b5cc8b5f186b9a84bf6f8f27b775
Leon Reteig <leonreteig@gmail.com>
2024-05-15 16:35:11 -0400

    Basic implementation of DSA

--- R/luminex.R
+++ R/luminex.R
@@ -12,3 +12,19 @@ determine_positive_beads <- function(specificities, mfi,
   ifelse(ratios > ratio_cutoff & mfi > mfi_cutoff, "positive", "negative") |>
     unname()
 }
+
+determine_dsa <- function(beads, typing,
+                          level = c("split", "broad"), sep = "\\s") {
+  # TODO: This works for now, but probably makes more sense if typing is
+  # also a vector of alleles. Then DSA is simply: beads == typing & positive.
+  level <- arg_match(level)
+  convert_typing <- switch(level,
+    split = get_serology,
+    broad = get_broad
+  )
+
+  typing_alleles <- stringr::str_split_1(typing[1], pattern = sep) |>
+    convert_typing()
+
+  ifelse(convert_typing(beads) %in% typing_alleles, "yes", "no")
+}
