5bc3ddcb2f89810180dc25655eba742166bf0aaa
Leon Reteig <leonreteig@gmail.com>
2024-05-15 16:34:43 -0400

    Basic implementation of bead positivity
    beadpos

---
+++ R/luminex.R
@@ -0,0 +1,14 @@
+determine_positive_beads <- function(specificities, mfi,
+                                     ratio_cutoff = 15, mfi_cutoff = 500) {
+  loci <- get_loci(specificities) # get locus of each bead
+
+  lra_mfis <- split(mfi, loci) |> # make one vector of MFI values per locus
+    purrr::map_dbl(min) # get the lowest MFI for each
+
+  # divide MFI of each bead by the MFI of the Lowest Ranked Antigen in its locus
+  ratios <- mfi / lra_mfis[loci]
+
+  # a bead is positive if both the ratio and raw values exceed the cutoff
+  ifelse(ratios > ratio_cutoff & mfi > mfi_cutoff, "positive", "negative") |>
+    unname()
+}
